04f2b48645149e5bbe06d4336bc9fae7192a2edb
aristotelesalves <arystotelys@gmail.com>
2024-08-25 14:48:15 -0300

    correção do csv

--- src/data/model.py
+++ src/data/model.py
@@ -13,7 +13,7 @@ def salvarJson(data):
 def salvarCSV(imoveis):
     data_e_hora_atual = DataHoraAtual()
     with open(f'imoveis-{data_e_hora_atual}.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['section', 'title', 'valor']
+        fieldnames = ['section', 'title', 'valor', 'quartos', 'banheiros', 'metros_quadrados', 'vagas_carro', 'endereço', 'data de postagem']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         
         writer.writeheader()
